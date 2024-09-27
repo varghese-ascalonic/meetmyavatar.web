752383c4afede8e777298773ee6bd7b7125fbde7
Varghese Mathai <varghesemathaime@gmail.com>
2024-09-27 20:18:16 +0200

    Initial commmit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <div class="text-center">
+      <h1 class="text-6xl font-bold text-gray-900 mb-6">Coming Soon</h1>
+      <p class="text-lg text-gray-600 mb-8">We're working hard to bring something awesome. Stay tuned!</p>
+      <div class="flex justify-center">
+        <input
+          type="email"
+          placeholder="Enter your email"
+          class="px-4 py-2 w-64 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+        />
+        <button class="ml-2 px-6 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700">
+          Notify Me
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "ComingSoon",
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
 </style>
